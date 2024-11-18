9b1532c673f4a7101b32981e8d776287eaefb6b4
AmbikaKachetti <ambika9339@gmail.com>
2024-11-18 23:59:01 +0530

    navbar updated

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+// import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Sidebar from './Components/Sidebar/Sidebar'
+import Admin from './Pages/Admin/Admin'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Admin/>
+      <Sidebar/>
     </div>
   )
 }
--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+// import React from 'react';
 import './Navbar.css'
-import navlogo from '../../assets/Admin_Assets/nav-logo.svg'
+import navlogo from '../../assets/Admin_Assets/big_logo.png'
 import navprofile from '../../assets/Admin_Assets/nav-profile.svg'
 
 const Navbar = () => {
     return (
         <div className='navbar'>
-        <img src={navlogo} alt="" className="nav_logo" />
-        <img src={navprofile} alt="" className='nav_profile'/>
+            <div className='navbaricon'>
+                <img src={navlogo} alt="" className="nav_logo" />
+                <h1>WearBy</h1>
+            </div>
+            <div>
+                <img src={navprofile} alt="" className='nav_profile'/>
+            </div>
         </div>
     )
 }
