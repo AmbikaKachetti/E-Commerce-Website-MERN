351f0b2249db25112c1aa744e1318f6aff744eb8
AmbikaKachetti <ambika9339@gmail.com>
2024-11-21 23:29:26 +0530

    admin updated

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -34,6 +34,17 @@ const AddProduct = () => {
         }).then((resp) => resp.json()).then((data)=>{responseData=data})
         if(responseData.success){
             product.image = responseData.image_url;
+            console.log(product);
+            await fetch('http://localhost:4000/addproduct',{
+                method: 'POST',
+                header: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(product),
+            }).then((resp)=>resp.json()).then((data)=>{
+                data.success?alert("Product Added"):alert("Failed")
+            })
         }
     }
     return (
